97fe123c072dc825a58f39a518b7950854d5da7b
Ryan Wong <r25wong@gmail.com>
2017-08-03 15:26:30 -0400

    added some stuff

--- SendSesEmail/config.js
+++ SendSesEmail/config.js
@@ -1,10 +1,10 @@
 ﻿"use strict";
 
 var config = {
-    "templateBucket" : "my-bucket",
+    "templateBucket" : "lambda-ses-email-template",
     "templateKey" : "Templates/Template.html",
-    "targetAddress" : "my-email@domain.com",
-    "fromAddress": "Me <my-email@domain.com>",
+    "targetAddress" : "contact@silkorb.com",
+    "fromAddress": "Jekyll Contact Form <jekyll@silkorb.com>",
     "defaultSubject" : "Email From {{name}}"
 }
 
